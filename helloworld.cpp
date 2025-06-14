ab3c1aeee6e58b55ac75a60481737cdab92fd96e
TheAxolotlLord <stevenshields618@gmail.com>
2025-06-14 11:29:49 -0600

    ayyy it works

---
+++ helloworld.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+int main() {
+
+    // intergers are whole numbers, which i know that
+    int age = 21;
+    int year = 2025;
+    int day = 7;
+
+    // double is like a float
+    double temp = 25.1;
+    double price = 10.49;
+
+    // a single char
+    char grade = 'A';
+    char favouriteLetter = '*';
+
+    // bool
+    bool alive = true;
+    bool on = false;
+
+    // better char
+    std::string whyIsItDifferent = "idk";
+
+    std::cout << whyIsItDifferent << '. you got a ' << grade << '\n';
+    std::cout << "Almost forgot to say Hello World!";
+
+    return 0;
+}
