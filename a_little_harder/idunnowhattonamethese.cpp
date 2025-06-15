539c2774eba97adf8de8931cbaae591622e67560
TheAxolotlLord <stevenshields618@gmail.com>
2025-06-15 10:44:55 -0600

    put something in here

--- a_little_harder/idunnowhattonamethese.cpp
+++ a_little_harder/idunnowhattonamethese.cpp
@@ -1,1 +1,20 @@
-// temporary file
+#include <iostream>
+#include <cmath>
+
+int main() {
+
+    // ternary operator
+
+    int number;
+
+    std::cout << "Enter a number: ";
+    std::cin >> number;
+
+    number % 2 == 0 ? std::cout << "Your number is even!" : std::cout << "Your number is odd!";
+
+    std::cout << "\n";
+
+    number <= 0 || number >= 100 ? std::cout << "Your number is not in a range from 1 to 100!" : std::cout << "Your number is in a range from 1 to 100!";
+
+    return 0;
+}
